0078609a94a6135e071f5f135536e393f2161af1
moeka817 <sum.flower17@gmail.com>
2025-11-19 22:17:33 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+</head>
+
+<body>
+    <p>
+        <?php
+        
+        $i = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+        print_r($i);
+
+        ?>
+    </p>
+</body>
+
+</html>
